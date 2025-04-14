d409113e8267920e4256c16373789d064123f2e9
mohaned5 <mohaned@live.co.uk>
2025-04-14 23:42:59 +0100

    Github pages compatible

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  distDir: 'out',
   images: {
     domains: ['upload.wikimedia.org'],
+    unoptimized: true,
   },
 };
 
